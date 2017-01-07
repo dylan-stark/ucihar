f757e3746f9e58c0a023c746f92524aedd83ebe2
Dylan Stark <dylan.stark@gmail.com>
2017-01-06 23:20:10 -0600

    Updates documentation.

--- R/data.R
+++ R/data.R
@@ -1,9 +1,27 @@
-#' Human Activity Recognition Using Smartphones
+#' Training data from Human Activity Recognition Using Smartphones data set
 #'
 #' Human activity data hosted at the UCI Machine Learning Repository.
 #'
 #' @source \url{http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones}
-"ucihar"
+"ucihar_training"
+
+#' Testing data from Human Activity Recognition Using Smartphones data set
+#'
+#' Human activity data hosted at the UCI Machine Learning Repository.
+#'
+#' @source \url{http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones}
+"ucihar_testing"
+
+#' Mean and standard deviation measurements from Human Activity Recognition Using Smartphones data set
+#'
+#' Human activity data hosted at the UCI Machine Learning Repository.
+#'
+#' @source \url{http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones}
+"ucihar_simple"
 
 #' Averages for Human Activity Recognition Using Smartphones
+#'
+#' Human activity data hosted at the UCI Machine Learning Repository.
+#'
+#' @source \url{http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones}
 "ucihar_avgs"
